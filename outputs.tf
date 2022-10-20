beed7cf36cbb2749fbafbb6b1801fe488a2395eb
Oleh Mykolaishyn <owlleg6@gmail.com>
2022-10-20 14:57:45 +0000

    feat: added output used by private dns module

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "name" {
   value       = azurerm_virtual_network.this.name
   description = "The name of the Virtual Network."
 }
+
+output "name_to_id_map" {
+  value       = { (azurerm_virtual_network.this.name) = azurerm_virtual_network.this.id }
+  description = "Map of Virtual Network Name to Id"
+}
