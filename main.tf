a0c2c528faf67c96dcefdb85c029b7fb0c54debd
owlleg6 <owlleg6@gmail.com>
2023-12-21 17:17:19 +0200

    feat: updated naming convention

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
-locals {
-  name   = var.custom_vnet_name == null ? "vnet-${var.project}-${var.env}-${var.location}" : var.custom_vnet_name
-  suffix = length(var.suffix) == 0 ? "" : "-${var.suffix}"
-}
 resource "azurerm_virtual_network" "this" {
-  name                = "${local.name}${local.suffix}"
+  name                = var.vnet_name
   address_space       = var.cidr
   location            = var.location
   resource_group_name = var.resource_group
